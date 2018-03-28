9c3f8ef53ece0e34cdfc764879828231a0f84fb7
Saketram Durbha <durbhasaketram@gmail.com>
2018-03-28 14:51:59 -0600

    Remove conflicting session entry

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,12 +1,12 @@
 module SessionsHelper
   # Logs in the given user.
   def log_in(user)
-    session[:user_id] = user.id
+    session[:logged_in_user_id] = user.id
   end
 
   # Returns the current logged-in user (if any).
   def current_user
-    @current_user ||= User.find_by(id: session[:user_id])
+    @current_user ||= User.find_by(id: session[:logged_in_user_id])
   end
 
   # Returns true if the user is logged in, false otherwise.
@@ -16,7 +16,7 @@ module SessionsHelper
 
   # Logs out the current user.
   def log_out
-    session.delete(:user_id)
+    session.delete(:logged_in_user_id)
     @current_user = nil
   end
 end
