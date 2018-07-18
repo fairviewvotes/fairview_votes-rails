78d9ba37a89c516c94e807d186af9f3fc370008b
Gernene <wallie988@gmail.com>
2018-07-17 23:45:37 -0600

    Start styling UI

---
+++ app/assets/javascripts/style.js
@@ -0,0 +1,7 @@
+window.onload = function changeBgGrad() {
+  var bgGradCount = 3;
+  var bgGradPref = "bg-grad";
+  var bgGradNum = Math.floor(Math.random() * bgGradCount);
+  var body = document.getElementsByTagName('body')[0];
+  body.className = bgGradPref + bgGradNum;
+}
