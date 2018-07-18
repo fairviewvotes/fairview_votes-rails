15efc6d905cb566bfc02d0cda4fd3780339e0de0
Gernene <wallie988@gmail.com>
2018-07-18 11:43:07 -0600

    Prep desktop css

--- app/assets/javascripts/style.js
+++ app/assets/javascripts/style.js
@@ -1,5 +1,5 @@
 window.onload = function changeBgGrad() {
-  var bgGradCount = 3;
+  var bgGradCount = 4;
   var bgGradPref = "bg-grad";
   var bgGradNum = Math.floor(Math.random() * bgGradCount);
   var body = document.getElementsByTagName('body')[0];
